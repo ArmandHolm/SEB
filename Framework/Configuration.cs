766d055fdf174acc27df072862497764b8f0f74a
ArmandHolm <73559808+ArmandHolm@users.noreply.github.com>
2020-10-27 23:27:33 +0200

    Add files via upload

---
+++ Framework/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SEB.Tests
+{
+    class Configuration
+    {
+        public static readonly TimeSpan DefaultBrowserDriverTimeout = TimeSpan.FromSeconds(120);
+
+        public static TimeSpan DefaultElementStatusCheckTimeout = TimeSpan.FromSeconds(90);
+
+        public static readonly TimeSpan DefaultPageLoadCheckStabilizationTimeout = TimeSpan.FromSeconds(2);
+    }
+}
